5279b84ac23551a792ddd2d5ff0cff1b0713baa8
Alex <gromvolk97@gmail.com>
2022-02-16 23:56:26 +0300

    helpers/added container component

--- src/pages/rp2-auth/login/index.tsx
+++ src/pages/rp2-auth/login/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import background_login from '../../../assets/images/background_auth.png'
-import s from './style.module.css'
-import InterestingFacts from "./InterestingFacts";
-import Authorization from "./authorization";
+import background_login from '../../../assets/images/background_auth.png';
+import s from './style.module.css';
+import InterestingFacts from './InterestingFacts';
+import Authorization from './authorization';
+import Container from '../../../helpers/container/container';
 
 const Login = () => {
-    return (
-        <div className={s.login} style={{backgroundImage: `url(${background_login})`}}>
-            <div className={s.wrapper}>
-                <InterestingFacts/>
-                <Authorization/>
-            </div>
-        </div>
-    );
+	return (
+		<div className={s.login} style={{ backgroundImage: `url(${background_login})` }}>
+			<Container>
+				<InterestingFacts />
+				<Authorization />
+			</Container>
+		</div>
+	);
 };
 
 export default Login;
