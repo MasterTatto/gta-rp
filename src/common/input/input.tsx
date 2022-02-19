dbbf04261ba9a312f6ea341dc331be8401cf6477
Alex <gromvolk97@gmail.com>
2022-02-19 03:33:50 +0300

    feat/regist/scale

--- src/common/input/input.tsx
+++ src/common/input/input.tsx
@@ -1,97 +1,111 @@
-import React, {ChangeEvent, KeyboardEvent, useEffect, useState} from 'react';
-import s from './style.module.css'
+import React, { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
+import s from './style.module.css';
+import validateSVG from '../../assets/icons/input-icons/validate.svg';
 
 type inputType = {
-    type: string
-    name: string
-    icon: string
-    placeholder: string
-    handleChange: (e: ChangeEvent<HTMLInputElement>) => void
-    value: string
-    errorMessage: string
-    required: boolean
-    pattern: string
-    showKeyBoardInfo?: boolean
-    className?: string
-    securityPassword?: boolean
-    securityPasswordNumber?: number
-}
+	type: string;
+	name: string;
+	icon: string;
+	placeholder: string;
+	handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+	value: string;
+	errorMessage: string;
+	required: boolean;
+	pattern: string;
+	showKeyBoardInfo?: boolean;
+	className?: string;
+	securityPassword?: boolean;
+	securityPasswordNumber?: number;
+	showValidateSvg?: boolean;
+};
 
 const Input = ({
-                   errorMessage,
-                   handleChange,
-                   value,
-                   type,
-                   className,
-                   icon,
-                   placeholder,
-                   showKeyBoardInfo,
-                   name,
-                   required,
-                   pattern,
-                   securityPassword,
-                   securityPasswordNumber
-               }: inputType) => {
-    const [showCaps, setShowCaps] = useState<boolean>(false)
-    const [showLanguage, setShowLanguage] = useState<string>('')
-    const [focused, setFocused] = useState<boolean>(false)
+	errorMessage,
+	handleChange,
+	value,
+	type,
+	className,
+	icon,
+	placeholder,
+	showKeyBoardInfo,
+	name,
+	required,
+	pattern,
+	securityPassword,
+	securityPasswordNumber,
+	showValidateSvg,
+}: inputType) => {
+	const [showCaps, setShowCaps] = useState<boolean>(false);
+	const [showLanguage, setShowLanguage] = useState<string>('');
+	const [focused, setFocused] = useState<boolean>(false);
 
-    const checkCapsAndLanguage = (event: KeyboardEvent<HTMLInputElement>) => {
-        const caps = event.getModifierState && event.getModifierState('CapsLock')
-        setShowCaps(caps)
+	const checkCapsAndLanguage = (event: KeyboardEvent<HTMLInputElement>) => {
+		const caps = event.getModifierState && event.getModifierState('CapsLock');
+		setShowCaps(caps);
 
-        const rusLowerCase = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-        const enLowercase = 'abcdefghijklmnopqrstuvwxyz'
-        const ru = rusLowerCase + rusLowerCase.toUpperCase()
-        const en = enLowercase + enLowercase.toUpperCase()
+		const rusLowerCase = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+		const enLowercase = 'abcdefghijklmnopqrstuvwxyz';
+		const ru = rusLowerCase + rusLowerCase.toUpperCase();
+		const en = enLowercase + enLowercase.toUpperCase();
 
-        if (ru.includes(event.key)) {
-            setShowLanguage('RU')
-        } else if (en.includes(event.key)) {
-            setShowLanguage('EN')
-        }
-    }
+		if (ru.includes(event.key)) {
+			setShowLanguage('RU');
+		} else if (en.includes(event.key)) {
+			setShowLanguage('EN');
+		}
+	};
 
-    const scaleSecurity = Array.from(Array(5).keys())
+	const scaleSecurity = Array.from(Array(5).keys());
+	console.log(securityPasswordNumber);
+	useEffect(() => {
+		const elements = document.querySelectorAll('#item');
 
-    useEffect(() => {
-        const elements = document.querySelectorAll('#item')
+		elements.forEach((el, i) => {
+			if (i < securityPasswordNumber!) {
+				el.classList.add(s.active);
+			}
+			if (i >= securityPasswordNumber!) {
+				el.classList.remove(s.active);
+			}
+		});
+	}, [securityPasswordNumber]);
 
-        elements.forEach((el, i) => {
-            if (i < securityPasswordNumber!) {
-                el.className = s.active
-            }
-
-        })
-    }, [securityPasswordNumber])
-
-    return (
-        <div className={`${s.input} ${className}`}>
-
-            <input pattern={pattern} required={required} type={type} placeholder={placeholder} value={value}
-                   onKeyUp={(event) => checkCapsAndLanguage(event)}
-                   name={name} onChange={(e) => handleChange(e)}
-                   onBlur={() => {
-                       setFocused(true)
-                       setShowLanguage('')
-                       setShowCaps(false)
-                   }} data-focused={focused.toString()}
-                   onFocus={() => name === 'confirmPassword' && setFocused(true)}
-            />
-            {securityPassword && <div className={s.scaleSecurity}>
-                <div className={s.scaleSecurity__box}>
-                    {scaleSecurity.map((el, i) => (
-                        <div key={i} id={'item'} className={`${s.scaleSecurity__item} }`}/>
-                    ))}
-                </div>
-                <span className={s.scaleSecurity__title}>легкий</span>
-            </div>}
-            {(showKeyBoardInfo && showCaps) && <p className={s.capsLock}>Caps Lock</p>}
-            {(showKeyBoardInfo && showLanguage) && <p className={s.language}>{showLanguage}</p>}
-            <span className={s.errorMessage}>{errorMessage}</span>
-            <img src={icon} alt=""/>
-        </div>
-    );
+	return (
+		<div className={`${s.input} ${className}`}>
+			<input
+				pattern={pattern}
+				required={required}
+				type={type}
+				placeholder={placeholder}
+				value={value}
+				onKeyUp={(event) => checkCapsAndLanguage(event)}
+				name={name}
+				onChange={(e) => handleChange(e)}
+				onBlur={() => {
+					setFocused(true);
+					setShowLanguage('');
+					setShowCaps(false);
+				}}
+				data-focused={focused.toString()}
+				onFocus={() => name === 'confirmPassword' && setFocused(true)}
+			/>
+			{securityPassword && (
+				<div className={s.scaleSecurity}>
+					<div className={s.scaleSecurity__box}>
+						{scaleSecurity.map((el, i) => (
+							<div key={i} id={'item'} className={`${s.scaleSecurity__item}`} />
+						))}
+					</div>
+					<span className={s.scaleSecurity__title}>легкий</span>
+				</div>
+			)}
+			{showValidateSvg && <img className={s.validateSVG} src={validateSVG} alt='' />}
+			{showKeyBoardInfo && showCaps && <p className={s.capsLock}>Caps Lock</p>}
+			{showKeyBoardInfo && showLanguage && <p className={s.language}>{showLanguage}</p>}
+			<span className={s.errorMessage}>{errorMessage}</span>
+			<img className={s.logo_input} src={icon} alt='' />
+		</div>
+	);
 };
 
 export default Input;
--- src/pages/rp2-auth/registration/index.tsx
+++ src/pages/rp2-auth/registration/index.tsx
@@ -1,122 +1,127 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
-import Input from "../../../common/input/input";
-import s from './style.module.css'
-import userIcon from '../../../assets/icons/input-icons/user.svg'
-import passwordIcon from '../../../assets/icons/input-icons/password.svg'
-import emailIcon from '../../../assets/icons/input-icons/email.svg'
-import promoIcon from '../../../assets/icons/input-icons/promo.svg'
-import Button from "../../../common/button/button";
-
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import Input from '../../../common/input/input';
+import s from './style.module.css';
+import userIcon from '../../../assets/icons/input-icons/user.svg';
+import passwordIcon from '../../../assets/icons/input-icons/password.svg';
+import emailIcon from '../../../assets/icons/input-icons/email.svg';
+import promoIcon from '../../../assets/icons/input-icons/promo.svg';
+import Button from '../../../common/button/button';
 
 const Registration = () => {
-    const [checkedMail, setCheckedMail] = useState(true)
-    const [checkedRoot, setCheckedRoot] = useState(false)
-    const [securityPasswordNumber, setSecurityPasswordNumber] = useState<number>(0)
+	const [checkedMail, setCheckedMail] = useState(true);
+	const [checkedRoot, setCheckedRoot] = useState(false);
+	const [securityPasswordNumber, setSecurityPasswordNumber] = useState<number>(0);
 
-    const [values, setValues] = useState({
-        userName: '',
-        password: '',
-        confirmPassword: '',
-        email: '',
-        promo: ''
-    })
+	const [values, setValues] = useState({
+		userName: '',
+		password: '',
+		confirmPassword: '',
+		email: '',
+		promo: '',
+	});
 
-    const inputs = [
-        {
-            name: 'userName',
-            type: 'text',
-            placeholder: 'Логин',
-            icon: userIcon,
-            errorMessage: 'Логин должен состоять от 4-16 символов',
-            pattern: "^[A-Za-z0-9-А-Яа-я]{4,16}$",
-            required: true
-        },
-        {
-            name: 'password',
-            type: 'password',
-            placeholder: 'Пароль',
-            icon: passwordIcon,
-            errorMessage: 'Пароль должен состоять от 6-20 символов',
-            showKeyBoardInfo: true,
-            pattern: "^[A-Za-z0-9-А-Яа-я!@#$%^&*]{6,20}$",
-            required: true,
-            securityPassword: true
-        },
-        {
-            name: 'confirmPassword',
-            type: 'password',
-            placeholder: 'Подтвердите пароль',
-            icon: passwordIcon,
-            errorMessage: 'Пароли не совпадают',
-            pattern: values.password,
-            required: true
-        },
-        {
-            name: 'email',
-            type: 'email',
-            placeholder: 'E-mail',
-            icon: emailIcon,
-            errorMessage: 'Невалидный email адресс',
-            pattern: `^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$`,
-            required: true
-        },
-        {
-            name: 'promo',
-            type: 'text',
-            placeholder: 'Промокод',
-            icon: promoIcon,
-            errorMessage: '',
-            required: true
-        },
-    ]
+	const inputs = [
+		{
+			name: 'userName',
+			type: 'text',
+			placeholder: 'Логин',
+			icon: userIcon,
+			errorMessage: 'Логин должен состоять от 4-16 символов',
+			pattern: '^[A-Za-z0-9-А-Яа-я]{4,16}$',
+			required: true,
+			showValidateSvg: /^[A-Za-z0-9-А-Яа-я]{4,16}$/.test(values.userName),
+		},
+		{
+			name: 'password',
+			type: 'password',
+			placeholder: 'Пароль',
+			icon: passwordIcon,
+			errorMessage: 'Пароль должен состоять от 6-20 символов',
+			showKeyBoardInfo: true,
+			pattern: '^[A-Za-z0-9-А-Яа-я!@#$%^&*]{6,20}$',
+			required: true,
+			securityPassword: true,
+			showValidateSvg: /^[A-Za-z0-9-А-Яа-я!@#$%^&*]{6,20}$/.test(values.password),
+		},
+		{
+			name: 'confirmPassword',
+			type: 'password',
+			placeholder: 'Подтвердите пароль',
+			icon: passwordIcon,
+			errorMessage: 'Пароли не совпадают',
+			pattern: values.password,
+			required: true,
+			showValidateSvg: values.password === values.confirmPassword && /^[A-Za-z0-9-А-Яа-я!@#$%^&*]{6,20}$/.test(values.password),
+		},
+		{
+			name: 'email',
+			type: 'email',
+			placeholder: 'E-mail',
+			icon: emailIcon,
+			errorMessage: 'Невалидный email адресс',
+			pattern: `^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$`,
+			required: true,
+			showValidateSvg: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(values.email),
+		},
+		{
+			name: 'promo',
+			type: 'text',
+			placeholder: 'Промокод',
+			icon: promoIcon,
+			errorMessage: '',
+			required: true,
+		},
+	];
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setValues({...values, [e.target.name]: e.target.value})
-    }
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setValues({ ...values, [e.target.name]: e.target.value });
+	};
 
-    useEffect(() => {
-        if (/[a-z0-9-а-я]/.test(values.password) && values.password.length >= 6) {
-            setSecurityPasswordNumber(1)
-        } else if (/[A-Za-z0-9-А-Яа-я]/.test(values.password) && values.password.length >= 6) {
-            setSecurityPasswordNumber(2)
-        } else if (/[A-Za-z0-9-А-Яа-я!@#$%^&*]/.test(values.password) && values.password.length >= 6) {
-            setSecurityPasswordNumber(3)
-        } else if (values.password.length < 6) {
-            setSecurityPasswordNumber(0)
-        }
+	useEffect(() => {
+		if (values.password === '111111') {
+			setSecurityPasswordNumber(1);
+		} else if (values.password === '1111111') {
+			setSecurityPasswordNumber(2);
+		} else if (/[A-Za-z0-9-А-Яа-я!@#$%^&*]/.test(values.password) && values.password.length >= 6) {
+			setSecurityPasswordNumber(3);
+		} else if (values.password.length < 6) {
+			setSecurityPasswordNumber(0);
+		}
+	}, [values.password]);
+	console.log(securityPasswordNumber);
 
-    }, [values.password, securityPasswordNumber])
-    console.log(securityPasswordNumber)
-
-
-    return (
-        <div>
-            <form>
-                {inputs.map((input, i) => (
-                    //@ts-ignore
-                    <Input key={input.name + i} value={values[input.name]} className={s.inputs_regist}
-                           securityPasswordNumber={securityPasswordNumber}
-                           handleChange={handleChange}  {...input}/>
-                ))}
-                <div className={s.checkbox_box}>
-                    <div className={s.checkbox_box__item}>
-                        <input type="checkbox" className={s.checkbox} checked={checkedMail}
-                               onChange={() => setCheckedMail(!checkedMail)}/>
-                        <span>Согласен принимать информационные рассылки</span>
-                    </div>
-                    <div className={s.checkbox_box__item}>
-                        <input type="checkbox" className={s.checkbox} checked={checkedRoot}
-                               onChange={() => setCheckedRoot(!checkedRoot)}/>
-                        <span>Я ознакомился с правилами сервера и принимаю их</span>
-                    </div>
-                </div>
-                <div className={s.button_box}>
-                    <Button title={'Правила сервера'} className={`${s.btn} ${s.root_btn}`}/>
-                    <Button title={'Создать аккаунт'} className={s.btn}/>
-                </div>
-            </form>
-        </div>
-    );
+	return (
+		<div>
+			<form>
+				{inputs.map((input, i) => (
+					//@ts-ignore
+					<Input
+						key={input.name + i}
+						//@ts-ignore
+						value={values[input.name]}
+						className={s.inputs_regist}
+						securityPasswordNumber={securityPasswordNumber}
+						handleChange={handleChange}
+						{...input}
+					/>
+				))}
+				<div className={s.checkbox_box}>
+					<div className={s.checkbox_box__item}>
+						<input type='checkbox' className={s.checkbox} checked={checkedMail} onChange={() => setCheckedMail(!checkedMail)} />
+						<span>Согласен принимать информационные рассылки</span>
+					</div>
+					<div className={s.checkbox_box__item}>
+						<input type='checkbox' className={s.checkbox} checked={checkedRoot} onChange={() => setCheckedRoot(!checkedRoot)} />
+						<span>Я ознакомился с правилами сервера и принимаю их</span>
+					</div>
+				</div>
+				<div className={s.button_box}>
+					<Button title={'Правила сервера'} className={`${s.btn} ${s.root_btn}`} />
+					<Button title={'Создать аккаунт'} className={s.btn} />
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Registration;
